d2447f4a0ecc247a21d5d975dfc421344f3b4b39
Parth Singh <114077978+Parth-Singhh@users.noreply.github.com>
2026-07-18 13:29:15 +0530

    Repair

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,6 @@ import { Module, McpApp } from '@nitrostack/core';
 import { ThreatAnalysisModule } from './modules/threatanalysis/threatanalysis.module.js';
 import { IncidentOpsModule } from './modules/incidentops/incidentops.module.js';
 import { IocAnalyzerModule } from './modules/iocanalyzer/iocanalyzer.module.js';
-import { PasswordAnalyzerModule } from './modules/passwordanalyzer/passwordanalyzer.module.js';
-import { EmailHeaderAnalyzerModule } from './modules/emailheaderanalyzer/emailheaderanalyzer.module.js';
-import { DomainIntelligenceModule } from './modules/domainintelligence/domainintelligence.module.js';
-import { FileRiskAnalyzerModule } from './modules/fileriskanalyzer/fileriskanalyzer.module.js';
-import { IncidentAdvisorModule } from './modules/incidentadvisor/incidentadvisor.module.js';
 
 @McpApp({
   module: AppModule,
@@ -17,15 +12,6 @@ import { IncidentAdvisorModule } from './modules/incidentadvisor/incidentadvisor
 })
 @Module({
   name: 'app',
-  imports: [
-    ThreatAnalysisModule,
-    IncidentOpsModule,
-    IocAnalyzerModule,
-    PasswordAnalyzerModule,
-    EmailHeaderAnalyzerModule,
-    DomainIntelligenceModule,
-    FileRiskAnalyzerModule,
-    IncidentAdvisorModule,
-  ],
+  imports: [ThreatAnalysisModule, IncidentOpsModule, IocAnalyzerModule],
 })
-export class AppModule {}
+export class AppModule {} 
